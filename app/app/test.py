50aaf834a5b135c51544f9398b6a8d94132797b6
Nathon Reed <nrwebdev00@gmail.com>
2022-08-06 09:22:02 -0700

    setup some testing - partice

--- app/app/test.py
+++ app/app/test.py
@@ -4,14 +4,14 @@ from app import clac
 
 
 class ClacTests(SimpleTestCase):
-    
+
     def test_add_numbers(self):
         res = clac.add(15, 20)
-        
+
         self.assertEqual(res, 35)
-        
+
     def test_subtract_number(self):
         res = clac.sub(15, 20)
-        
+
         self.assertEqual(res, -5)
         
